a25ec174dcabcbd7b0b15efea1f83cde368c1e3e
MarcoChavezFdez <mchavez297@accitesz.com>
2021-02-26 12:40:17 -0600

    Modelo RH_Estados

--- IERP_RH/src/modelo/RH_Estados.java
+++ IERP_RH/src/modelo/RH_Estados.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Modelo de Estados
  */
 package modelo;
 
@@ -10,5 +11,42 @@ package modelo;
  * @author Marco Chavez
  */
 public class RH_Estados {
+    private Integer idCiudad;
+    private String nombre;
+    private Integer idEstado;
+    private char estatus;
+
+    public Integer getIdCiudad() {
+        return idCiudad;
+    }
+
+    public void setIdCiudad(Integer idCiudad) {
+        this.idCiudad = idCiudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdEstado() {
+        return idEstado;
+    }
+
+    public void setIdEstado(Integer idEstado) {
+        this.idEstado = idEstado;
+    }
+
+    public char getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(char estatus) {
+        this.estatus = estatus;
+    }
+    
     
 }
