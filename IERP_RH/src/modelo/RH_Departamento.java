fc3bce479b70112e215a34491dcf0787a73c109a
MarcoChavezFdez <mchavez297@accitesz.com>
2021-04-21 18:30:54 -0500

    Create RH_Departamento.java
    Modelo para la tabla de RH.Departamentos

---
+++ IERP_RH/src/modelo/RH_Departamento.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Marco Chavez
+ */
+public class RH_Departamento {
+    private Integer idDepartamento;
+    private String nombre;
+    private String estatus;
+
+    public RH_Departamento() {
+    }
+
+    public Integer getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(Integer idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+    
+    
+}
