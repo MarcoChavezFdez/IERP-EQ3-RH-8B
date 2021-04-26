bdd5f5267be7a646ec84e71b5c5015131e118d49
CarlosdeAnda <jesparzaz301@accitesz.com>
2021-04-26 00:03:24 -0500

    Revert "a"
    This reverts commit 7df9f1ed28c181246279b112acb6e659388d8dc5.

--- IERP_RH/src/GUI/TurnosFrame.java
+++ IERP_RH/src/GUI/TurnosFrame.java
@@ -43,7 +43,6 @@ public class TurnosFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         txtBusqueda = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -55,25 +54,18 @@ public class TurnosFrame extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(241, 151, 89));
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Assets/btnAtras.png"))); // NOI18N
-        jButton1.setBorderPainted(false);
-        jButton1.setContentAreaFilled(false);
+        jButton1.setText("ATRAS");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-10, -10, -1, -1));
 
         txtBusqueda.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyReleased(java.awt.event.KeyEvent evt) {
                 txtBusquedaKeyReleased(evt);
             }
         });
-        jPanel1.add(txtBusqueda, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 80, 251, -1));
 
         tbl_Datos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -93,10 +85,7 @@ public class TurnosFrame extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(tbl_Datos);
 
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 104, 929, -1));
-
         btnbuscar.setText("Buscar");
-        jPanel1.add(btnbuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 70, -1, -1));
 
         btninsertar.setText("Insertar");
         btninsertar.addActionListener(new java.awt.event.ActionListener() {
@@ -104,7 +93,6 @@ public class TurnosFrame extends javax.swing.JFrame {
                 btninsertarActionPerformed(evt);
             }
         });
-        jPanel1.add(btninsertar, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 60, 75, -1));
 
         btn_Modificar.setText("Modificar");
         btn_Modificar.setEnabled(false);
@@ -113,7 +101,6 @@ public class TurnosFrame extends javax.swing.JFrame {
                 btn_ModificarActionPerformed(evt);
             }
         });
-        jPanel1.add(btnmodificar, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 70, -1, -1));
 
         btn_Eliminar.setText("Eliminar");
         btn_Eliminar.setEnabled(false);
@@ -122,14 +109,12 @@ public class TurnosFrame extends javax.swing.JFrame {
                 btn_EliminarActionPerformed(evt);
             }
         });
-        jPanel1.add(btneliminar, new org.netbeans.lib.awtextra.AbsoluteConstraints(930, 70, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-<<<<<<< Updated upstream
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
@@ -151,15 +136,10 @@ public class TurnosFrame extends javax.swing.JFrame {
                                 .addComponent(btnbuscar)))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
-=======
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
->>>>>>> Stashed changes
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-<<<<<<< Updated upstream
                 .addGap(32, 32, 32)
                 .addComponent(jButton1)
                 .addGap(29, 29, 29)
@@ -176,10 +156,6 @@ public class TurnosFrame extends javax.swing.JFrame {
                         .addGap(33, 33, 33)
                         .addComponent(btn_Eliminar)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-=======
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(106, Short.MAX_VALUE))
->>>>>>> Stashed changes
         );
 
         pack();
@@ -307,7 +283,6 @@ public class TurnosFrame extends javax.swing.JFrame {
     private javax.swing.JButton btnbuscar;
     private javax.swing.JButton btninsertar;
     private javax.swing.JButton jButton1;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tbl_Datos;
     private javax.swing.JTextField txtBusqueda;
