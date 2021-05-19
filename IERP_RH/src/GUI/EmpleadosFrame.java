767d3bd665ac020eb2e23379d517f9648dcd06aa
MarcoChavezFdez <mchavez297@accitesz.com>
2021-05-18 23:24:15 -0500

    Update EmpleadosFrame.java

--- IERP_RH/src/GUI/EmpleadosFrame.java
+++ IERP_RH/src/GUI/EmpleadosFrame.java
@@ -221,36 +221,6 @@ public class EmpleadosFrame extends javax.swing.JFrame {
 
     }//GEN-LAST:event_btn_ModificarActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EmpleadosFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EmpleadosFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EmpleadosFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(EmpleadosFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-    }
-
     private void llenarTabla(ArrayList<RH_Empleado> lista) {
         String[] encabezado = {"IdEmpleado", "Nombre", "Apellido Paterno", "Apellido Materno", "Sexo", "Fecha Nacimiento", "CURP", "estadoCivil", "Fecha Contratacion", "Salario Diario", "NSS", "Dias Vacaciones", "Dias Permiso", "Direccion", "Colonia", "Codigo Postal", "Escolaridad", "Especialidad", "Email", "Tipo", "Departamento", "Puesto", "Ciudad", "Sucursal", "Turno"};
         Object[][] datos = new Object[lista.size()][26];
