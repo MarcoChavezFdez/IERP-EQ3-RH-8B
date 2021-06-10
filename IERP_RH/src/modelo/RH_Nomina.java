5820321a9ec50b067dfb04c3bbbede9b19c275b8
MarcoChavezFdez <mchavez297@accitesz.com>
2021-06-10 00:35:30 -0500

    Update RH_Nomina.java
    Creacion del Metodo calculaNomina para calcular la Nomina

--- IERP_RH/src/modelo/RH_Nomina.java
+++ IERP_RH/src/modelo/RH_Nomina.java
@@ -8,12 +8,14 @@ package modelo;
 import conexion.ConexionBD;
 import conexion.NominaDAO;
 import java.sql.Date;
+import java.util.ArrayList;
 
 /**
  *
  * @author Marco Chavez
  */
 public class RH_Nomina {
+
     private Integer idNomina;
     private java.sql.Date fechaElaboracion;
     private java.sql.Date fechaPago;
@@ -28,22 +30,22 @@ public class RH_Nomina {
 
     public RH_Nomina() {
     }
-    
-    public void recuperaNomina(ConexionBD conexion){
+
+    public void recuperaNomina(ConexionBD conexion) {
         NominaDAO dao = new NominaDAO(conexion);
         RH_Nomina nomina = new RH_Nomina();
         nomina = dao.consultaNominaId(this.idNomina);
-        this.fechaElaboracion=nomina.getFechaElaboracion();
-        this.fechaPago=nomina.getFechaPago();
-        this.subtotal=nomina.getSubtotal();
-        this.retenciones=nomina.getRetenciones();
-        this.total=nomina.getTotal();
-        this.diasTrabajados=nomina.getDiasTrabajados();
-        this.Estatus=nomina.getEstatus();
-        this.empleado=nomina.getEmpleado();
-        this.formaPago=nomina.getFormaPago();
-        this.periodo =nomina.getPeriodo();
-        
+        this.fechaElaboracion = nomina.getFechaElaboracion();
+        this.fechaPago = nomina.getFechaPago();
+        this.subtotal = nomina.getSubtotal();
+        this.retenciones = nomina.getRetenciones();
+        this.total = nomina.getTotal();
+        this.diasTrabajados = nomina.getDiasTrabajados();
+        this.Estatus = nomina.getEstatus();
+        this.empleado = nomina.getEmpleado();
+        this.formaPago = nomina.getFormaPago();
+        this.periodo = nomina.getPeriodo();
+
     }
 
     public Integer getIdNomina() {
@@ -133,6 +135,23 @@ public class RH_Nomina {
     public void setPeriodo(RH_Periodo periodo) {
         this.periodo = periodo;
     }
-    
-           
+
+    public void calculaNomina(ArrayList<RH_Percepcion> percepciones, ArrayList<RH_Deduccion> deducciones) {
+        this.subtotal = 0.f;
+        this.subtotal += this.empleado.getSalarioDiario() * this.getDiasTrabajados();
+
+        percepciones.forEach((p) -> {
+            this.subtotal += p.getDiasPagar() * this.empleado.getSalarioDiario();
+        });
+
+        this.retenciones = 0.0f;
+        deducciones.forEach((d) -> {
+            this.retenciones +=d.getPorcentaje();
+        });
+
+        this.retenciones /= 100;
+        this.retenciones *= this.subtotal;
+        this.total = this.subtotal - this.retenciones;
+    }
+
 }
