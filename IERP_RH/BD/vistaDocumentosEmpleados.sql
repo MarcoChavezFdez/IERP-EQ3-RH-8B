62bd7770c2d44a74ff9cf32ec340606e9623d9aa
MarcoChavezFdez <mchavez297@accitesz.com>
2021-06-09 20:20:43 -0500

    Actualización de los Recursos de la Base de Datos
    Actualización del script del esquema y objetos de la base de datos
    Eliminación de archivos redundantes

--- IERP_RH/BD/vistaDocumentosEmpleados.sql
+++
@@ -1,5 +0,0 @@
-SELECT DocEmpleado.idDocumento,DocEmpleado.nombreDocumento,DocEmpleado.fechaEntrega,CONCAT(Em.nombre,' ',Em.apellidoPaterno,' ',Em.apellidoMaterno)as NombreEmpleado FROM 
-RH.DocumentacionEmpleado DocEmpleado
-join RH.Empleados Em on
-Em.idEmpleado=DocEmpleado.idEmpleado
-where Em.estatus='A' and DocEmpleado.estatus='A'
