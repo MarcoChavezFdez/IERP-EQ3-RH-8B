9462db89f1e94ce736848a0a9b8a7581ad49adad
MarcoChavezFdez <mchavez297@accitesz.com>
2021-06-09 20:14:03 -0500

    Update AddDocumentacionEmpleado
    Modificación para mostrar los documentos de la Documentacion del Empleado

--- IERP_RH/src/GUI/AddDocumentacionEmpleado.java
+++ IERP_RH/src/GUI/AddDocumentacionEmpleado.java
@@ -8,18 +8,16 @@ package GUI;
 import conexion.ConexionBD;
 import conexion.DocumentacionEmpleadoDAO;
 import conexion.EmpleadoDAO;
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import modelo.RH_DocumentacionEmpleado;
@@ -38,6 +36,7 @@ public class AddDocumentacionEmpleado extends javax.swing.JFrame {
     ArrayList<RH_Empleado> empleados = new ArrayList();
     Boolean bandera;
     Boolean isNew;
+    String path = "";
 
     /**
      * Creates new form AddPuestos
@@ -50,6 +49,7 @@ public class AddDocumentacionEmpleado extends javax.swing.JFrame {
         this.conexion = conexion;
         lbl_Titulo.setText("Añadir Documentación Empleado");
         btn_Realizar.setText("");
+        this.txf_FechaDocumento.setText(String.valueOf(java.sql.Date.valueOf(LocalDate.now())));
         this.bandera = false;
         this.isNew = true;
     }
@@ -57,9 +57,13 @@ public class AddDocumentacionEmpleado extends javax.swing.JFrame {
     public AddDocumentacionEmpleado(ConexionBD conexion, RH_DocumentacionEmpleado documentacion) {
         initComponents();
         this.setLocationRelativeTo(null);
+        this.conexion = conexion;
         this.documentacionEmpleado = documentacion;
         documento = this.documentacionEmpleado.getDocumento();
-        this.conexion = conexion;
+        txf_Nombre.setText(documentacion.getNombreDocumento());
+        txf_FechaDocumento.setText(String.valueOf(documentacion.getFechaEntrega().toLocalDate()));
+        this.txf_FechaDocumento.setText(String.valueOf(this.documentacionEmpleado.getFechaEntrega()));
+        creaArchivo();
         lbl_Titulo.setText("Modificar Documentación Empleado");
         btn_Realizar.setText("");
         btn_Realizar.setEnabled(true);
@@ -86,11 +90,11 @@ public class AddDocumentacionEmpleado extends javax.swing.JFrame {
         btn_Documento = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        dp_FechaEntrega = new com.github.lgooddatepicker.components.DatePicker();
         btn_Ver = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         cmb_Empleado = new javax.swing.JComboBox<>();
+        txf_FechaDocumento = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -164,14 +168,16 @@ public class AddDocumentacionEmpleado extends javax.swing.JFrame {
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 230, -1, 20));
 
         jLabel3.setText("Fecha Documento");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 280, -1, -1));
-
-        dp_FechaEntrega.setBackground(new java.awt.Color(153, 255, 153));
-        jPanel1.add(dp_FechaEntrega, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 280, -1, -1));
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 300, -1, -1));
 
         btn_Ver.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Assets/Documentacion/Ver.png"))); // NOI18N
         btn_Ver.setBorderPainted(false);
         btn_Ver.setContentAreaFilled(false);
+        btn_Ver.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_VerActionPerformed(evt);
+            }
+        });
         jPanel1.add(btn_Ver, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 220, 180, 50));
 
         jLabel4.setText("Documentación de Empleados");
@@ -184,6 +190,10 @@ public class AddDocumentacionEmpleado extends javax.swing.JFrame {
         cmb_Empleado.setBackground(new java.awt.Color(153, 255, 153));
         jPanel1.add(cmb_Empleado, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 350, 300, 30));
 
+        txf_FechaDocumento.setText("dd/mm/yyyy");
+        txf_FechaDocumento.setEnabled(false);
+        jPanel1.add(txf_FechaDocumento, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 290, 160, -1));
+
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 11, 660, 599));
 
         pack();
@@ -193,26 +203,38 @@ public class AddDocumentacionEmpleado extends javax.swing.JFrame {
         DocumentacionEmpleadoDAO documentacionDAO = new DocumentacionEmpleadoDAO(this.conexion);
         RH_DocumentacionEmpleado documentacion = new RH_DocumentacionEmpleado();
         documentacion.setNombreDocumento(txf_Nombre.getText());
-        documentacion.setFechaEntrega(java.sql.Date.valueOf(dp_FechaEntrega.getDate()));
+        documentacion.setFechaEntrega(java.sql.Date.valueOf(txf_FechaDocumento.getText()));
         documentacion.setDocumento(documento);
         documentacion.setEmpleado(empleados.get(cmb_Empleado.getSelectedIndex() - 1));
         documentacion.setEstatus("A");
         if (!isNew) {
             documentacion.setIdDocumento(this.documentacionEmpleado.getIdDocumento());
             if (documentacionDAO.actualizarDocumentacionEmpleado(documentacion)) {
-
+                JOptionPane.showMessageDialog(null, "Documentacion Modificada con exito");
             } else {
-
+                JOptionPane.showMessageDialog(null, "Ocurrio un error al intentar modificar la documentacion");
             }
         } else {
             if (documentacionDAO.insertarDocumentacionEmpleado(documentacion)) {
+                JOptionPane.showMessageDialog(null, "Documentacion creada con exito");
 
             } else {
-
+                JOptionPane.showMessageDialog(null, "Sucedio un error al intentar crear la documentacion");
             }
         }
+        DocumentacionEmpleadoFrame documentacionEmpleado = new DocumentacionEmpleadoFrame(this.conexion);
+        this.dispose();
+        documentacionEmpleado.setVisible(true);
+        this.pack();
     }//GEN-LAST:event_btn_RealizarActionPerformed
-
+    private void creaArchivo() {
+        try {
+            path = (new File(".").getCanonicalPath());
+            FileUtils.writeByteArrayToFile(new File(path + "\\resources\\docEmpleado.pdf"), this.documento);
+        } catch (IOException ex) {
+            Logger.getLogger(AddAusenciaJustificadaFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     private void btn_AtrasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_AtrasActionPerformed
         PrincipalFrame principal = new PrincipalFrame(this.conexion);
         this.dispose();
@@ -233,8 +255,8 @@ public class AddDocumentacionEmpleado extends javax.swing.JFrame {
         fileChooser.setFileFilter(new FileNameExtensionFilter("Documentos", "pdf"));
         fileChooser.showOpenDialog(fileChooser);
         try {
-            String ruta = fileChooser.getSelectedFile().getAbsolutePath();
             documento = FileUtils.readFileToByteArray(fileChooser.getSelectedFile());
+            creaArchivo();
         } catch (NullPointerException e) {
             System.out.println("No se ha seleccionado ningún fichero");
 
@@ -261,6 +283,16 @@ public class AddDocumentacionEmpleado extends javax.swing.JFrame {
     private void formMouseMoved(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseMoved
         verificarCampos();
     }//GEN-LAST:event_formMouseMoved
+
+    private void btn_VerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_VerActionPerformed
+        try {
+            path = (new File(".").getCanonicalPath());
+            Process p = Runtime.getRuntime().exec("rundll32 SHELL32.DLL,ShellExec_RunDLL " + path + "\\resources\\docEmpleado.pdf");
+        } catch (Exception evvv) {
+            JOptionPane.showMessageDialog(null, "No se puede abrir el archivo de ayuda, probablemente fue borrado", "ERROR", JOptionPane.ERROR_MESSAGE);
+
+        }
+    }//GEN-LAST:event_btn_VerActionPerformed
     public void verificarCampos() {
 
         if (!"".equals(txf_Nombre.getText()) && cmb_Empleado.getSelectedIndex() > 0 && !Objects.isNull(documento)) {
@@ -279,7 +311,6 @@ public class AddDocumentacionEmpleado extends javax.swing.JFrame {
     private javax.swing.JButton btn_Realizar;
     private javax.swing.JButton btn_Ver;
     private javax.swing.JComboBox<String> cmb_Empleado;
-    private com.github.lgooddatepicker.components.DatePicker dp_FechaEntrega;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -288,6 +319,7 @@ public class AddDocumentacionEmpleado extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lbl_Mensaje;
     private javax.swing.JLabel lbl_Titulo;
+    private javax.swing.JTextField txf_FechaDocumento;
     private javax.swing.JTextField txf_Nombre;
     // End of variables declaration//GEN-END:variables
 }
